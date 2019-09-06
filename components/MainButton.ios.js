c0ce93feed2a75f27baa434841fff05a1d55474e
Jay T <jaytula@gmail.com>
2019-09-06 08:07:29 -0700

    Finish Lesson 100: Platform-specific files

---
+++ components/MainButton.ios.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import Colors from "../constants/colors";
+
+const MainButton = props => {
+  return (
+    <View>
+      <TouchableOpacity activeOpacity={0.8} onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25
+  },
+  buttonText: {
+    color: "white",
+    fontFamily: "open-sans",
+    fontSize: 18
+  }
+});
+
+export default MainButton;

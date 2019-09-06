70f70e75f1c6da6407da10534ae111231587dbf1
Jay T <jaytula@gmail.com>
2019-09-05 21:24:24 -0700

    Finish Lesson 90

--- components/GameOverScreen.js
+++ components/GameOverScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  ScrollView
+} from "react-native";
 
 import TitleText from "./TitleText";
 import BodyText from "./BodyText";
@@ -9,28 +16,30 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = props => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          resizeMode="cover"
-          source={require("../assets/success.png")}
-          /*source={{
-            uri:
-              "https://abrahamswallet.com/wp-content/uploads/2017/12/samuel-ferrara-117219-1180x770.jpg"
-          }}*/
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            resizeMode="cover"
+            source={require("../assets/success.png")}
+            /*source={{
+              uri:
+                "https://abrahamswallet.com/wp-content/uploads/2017/12/samuel-ferrara-117219-1180x770.jpg"
+            }}*/
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{" "}
+            <Text style={styles.highlight}>{props.rounds}</Text> round to guess
+            the number <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onNewGame}>New Game</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed <Text style={styles.highlight}>{props.rounds}</Text>{" "}
-          round to guess the number{" "}
-          <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onNewGame}>New Game</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -45,19 +54,22 @@ const styles = StyleSheet.create({
     height: "100%"
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     borderColor: "black",
     overflow: "hidden",
-    marginVertical: 30
+    marginVertical: Dimensions.get("window").height / 30
   },
   resultContainer: {
-    marginVertical: 15,
+    marginVertical: Dimensions.get("window").height / 60,
     marginHorizontal: 30
   },
-  resultText: { fontSize: 20, textAlign: "center" },
+  resultText: {
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
+    textAlign: "center"
+  },
   highlight: {
     color: Colors.primary,
     fontFamily: "open-sans-bold"

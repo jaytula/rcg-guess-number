d1689d745abd6a35df37c6d26676d96e294c5d27
Jay T <jaytula@gmail.com>
2019-09-06 07:57:29 -0700

    Finish Lesson 99: Working with Platform

--- components/Header.js
+++ components/Header.js
@@ -8,22 +8,37 @@ const Header = props => {
   const { title } = props;
 
   return (
-    <View style={styles.root}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid
+        })
+      }}
+    >
       <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  root: {
+  headerBase: {
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
     height: 90,
-    paddingTop: 36,
-    backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
-    borderBottomColor: Platform.OS === "android" ? "#ccc" : "transparent",
-    borderBottomWidth: Platform.OS === "android" ? 2 : 0
+    paddingTop: 36
+  },
+  headerIOS: {
+    backgroundColor: "white",
+    borderBottomColor: "transparent",
+    borderBottomWidth: 0
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 2
   },
   text: {
     color: Platform.OS === "android" ? "white" : Colors.primary
